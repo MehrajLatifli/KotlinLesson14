461676606559dd47077718e7a97c2f3987d33cf4
MehrajLatifli <meracletifli@gmail.com>
2024-04-19 18:44:01 +0400

    GOD bless universes

--- src/main/kotlin/Task3/Human.kt
+++ src/main/kotlin/Task3/Human.kt
@@ -2,17 +2,22 @@ package Task3
 
 data class Human(var height: Double? = null, var gender: Gender? = null) {
 
-    fun calculateWeight(): Double {
+    fun calculateWeight(): Double? {
         val currentHeight = height ?: return 0.0
 
-        return when (gender) {
-            Gender.Male -> {
-                48.0 + (currentHeight - 150.0) / 2.5 * 2.7
+        return currentHeight?.let { c ->
+
+            when (gender) {
+                Gender.Male -> {
+                    48.0 + (c - 150.0) / 2.5 * 2.7
+                }
+
+                Gender.Female -> {
+                    45.0 + (c - 150.0) / 2.5 * 2.2
+                }
+
+                else -> 0.0
             }
-            Gender.Female -> {
-                45.0 + (currentHeight - 150.0) / 2.5 * 2.2
-            }
-            else -> 0.0
         }
     }
 }
--- src/main/kotlin/Task4/Task4.kt
+++ src/main/kotlin/Task4/Task4.kt
@@ -8,21 +8,25 @@ fun main(args: Array<String>) {
     print("\nEnter: ")
     var price = scanner.nextDouble()
 
-    println("Discount: " + calculateDiscount(price))
+    println("Price: " + calculateDiscount(price))
 
 }
 
 
-fun calculateDiscount(price: Double): Double {
-    return when {
-        price < 200.0 -> {
-            price * 0.9
+fun calculateDiscount(price: Double?=null): Double? {
+    return price?.let { p ->
+    when {
+        p < 200.0 -> {
+            p * 0.9
         }
-        price >= 200.0 -> {
-            price * 0.8
+
+        p >= 200.0 -> {
+            p * 0.8
         }
+
         else -> {
-            price
+            p
         }
+    }
     }
 }
